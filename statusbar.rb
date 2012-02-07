525263a10a7902f96d041947c5a1524aa2f18c2a
nate <n8@rockpapergoat.com>
2012-02-07 16:06:55 -0500

    tested preference file loading
    seems to work as hoped. i need to add bits to pull the prefs to instance
    variables. are symbols better here?

--- statusbar.rb
+++ statusbar.rb
@@ -9,6 +9,7 @@ def self.set_menu_items
     @year = Time.now.year.to_s
     @title = 'Troll-o-matic'
     @author = 'nate'
+    @icon = "trollface_big.png"
     @about = ["Troll-o-matic\nbendable pliers, inc.\n#{@year}", @author, "Thanks"]
     @menus = {"Update" => "update:", "Display" => "display:", "About #{@title}" => "about:"}
 end
@@ -41,14 +42,15 @@ end
 #
 # Menu Item Actions
 #
-def self.about(sender,about)
+def self.about(sender,about,icon)
     about = @about
+    icon = @icon
     alert = NSAlert.new
     alert.messageText = about[0]
     alert.informativeText = about[1]
     alert.alertStyle = NSInformationalAlertStyle
     alert.addButtonWithTitle(about[2])
-    alert.setIcon(NSImage.new.initWithContentsOfFile "trollface_big.png")
+    alert.setIcon(NSImage.new.initWithContentsOfFile icon)
     response = alert.runModal
 end
 
@@ -61,11 +63,11 @@ end
 
 # get prefs
 
-def self.get_prefs()
-  # test whether prefs exist
+def self.get_prefs(prefs)
+  # test whether prefs exist here or outside at initialization?
   # if not, set_prefs with some defaults, then read them
   # https://developer.apple.com/library/mac/#documentation/Cocoa/Conceptual/UserDefaults/Introduction/Introduction.html
-  # @config = load_plist File.read("/Library/Preferences/pref.plist")
+  @config = load_plist File.read(prefs)
   # read: NSDictionary.dictionaryWithContentsOfFile
 end
 
